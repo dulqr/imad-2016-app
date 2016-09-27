0788330a4e614fb697eb2e1ae6b9d01eacd8d74d
dulqr <gokzkrish009@gmail.com>
2016-09-27 21:42:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-txt');
 element.innerHTML='CN I HELP YOU';
+
+//move the image
+var img=document.getElementById('madi');
+img.onclick.function (); {
+    img.style.marginLeft='px';
+}
