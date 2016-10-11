49e3aca9c07578c60b2b20f361204c1220fc7297
dulqr <gokzkrish009@gmail.com>
2016-10-11 21:48:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone= {
+    title: 'article one|gokz',
+    heading: 'content of ma article!',
+    date: 'october 10 2016',
+    content:
+        `<p>
+            hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article. hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article.hey..!itz me yours gokul..this is ma first article.
+        </p>
+        <p>hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article. hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article.hey..!itz me yours gokul..this is ma first article.
+        </p>
+        <p>hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article. hey..!itz me yours gokul..this is ma first articlehey..!itz me yours gokul..this is ma first article.hey..!itz me yours gokul..this is ma first article.
+        </p>`
+};
+
+function createtemplate(data){
+   var title = data.title;
+   var heading = data.heading;
+   var date = data.date;
+   var content = data.content;
+   
+   var htmlTemplate=`
+            <html>
+                <head>
+                    <title>
+                        ${title}
+                    </title>
+                    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                    <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <div class="hume">
+                <div>
+                    <a href="/">HOME</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <hr/>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content} 
+                </div>
+                </div>
+                </body>
+            </html>
+    `;
+return htmlTemplate;
+}
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemlate(articleOne));
 });
 app.get('/article-two',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
