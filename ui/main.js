c882026fbb374a971c2c4ff7f7157225681bb6fa
dulqr <gokzkrish009@gmail.com>
2016-10-12 21:36:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,2 @@
 console.log('Loaded!');
 
-//change the text of the main-txt div
-
-var element=document.getElementById('main-txt');
-element.innerHTML='CN I HELP YOU';
-
-//move the image
-var img=document.getElementById('madi');
-img.onclick.function (); {
-    img.style.marginLeft='10';
-}
